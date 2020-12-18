d82ceb553abfe2d4e451c4bbe1324390c8849b97
Tisks <leiser.mahu@usach.com>
2020-12-17 23:02:11 -0300

    Addding and adaption v-dashboard from wobsoriano/v-dashboard github template

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,22 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view />
+  <component :is="layout">
+    <router-view />
+  </component>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<script lang="ts">
+import { defineComponent, ref, computed } from "vue";
+import { useRouter } from "vue-router";
+const defaultLayout = "default";
+export default defineComponent({
+  setup() {
+    const { currentRoute } = useRouter();
+    const layout = computed(
+      () => `${currentRoute.value.meta.layout || defaultLayout}-layout`
+    );
+    return {
+      layout,
+    };
+  },
+});
+</script>
