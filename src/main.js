841151ad008cbb9343aca1b522b8f08915429c81
Tisks <leiser.mahu@usach.cl>
2021-01-27 14:28:53 -0300

    Reusing Buefy basic admin dashboard
    Uses Vue 2
    
    https://github.com/vikdiesel/admin-one-vue-bulma-dashboard

--- src/main.js
+++ src/main.js
@@ -1,9 +1,37 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+/* Styles */
+import '@/scss/main.scss'
 
-createApp(App)
-  .use(store)
-  .use(router)
-  .mount("#app");
+/* Core */
+import Vue from 'vue'
+import Buefy from 'buefy'
+
+/* Router & Store */
+import router from './router'
+import store from './store'
+
+/* Vue. Main component */
+import App from './App.vue'
+
+/* Default title tag */
+const defaultDocumentTitle = 'Admin One Bulma'
+
+/* Collapse mobile aside menu on route change & set document title from route meta */
+router.afterEach(to => {
+  store.commit('asideMobileStateToggle', false)
+
+  if (to.meta && to.meta.title) {
+    document.title = `${to.meta.title} — ${defaultDocumentTitle}`
+  } else {
+    document.title = defaultDocumentTitle
+  }
+})
+
+Vue.config.productionTip = false
+
+Vue.use(Buefy)
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,77 @@
-import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
+    // Document title tag
+    // We combine it with defaultDocumentTitle set in `src/main.js` on router.afterEach hook
+    meta: {
+      title: 'Dashboard'
+    },
+    path: '/',
+    name: 'home',
     component: Home
   },
   {
-    path: "/about",
-    name: "About",
+    meta: {
+      title: 'Tables'
+    },
+    path: '/tables',
+    name: 'tables',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    component: () => import(/* webpackChunkName: "tables" */ '../views/Tables.vue')
+  },
+  {
+    meta: {
+      title: 'Forms'
+    },
+    path: '/forms',
+    name: 'forms',
+    component: () => import(/* webpackChunkName: "forms" */ '../views/Forms.vue')
+  },
+  {
+    meta: {
+      title: 'Profile'
+    },
+    path: '/profile',
+    name: 'profile',
+    component: () => import(/* webpackChunkName: "profile" */ '../views/Profile.vue')
+  },
+  {
+    meta: {
+      title: 'New Client'
+    },
+    path: '/client/new',
+    name: 'client.new',
+    component: () => import(/* webpackChunkName: "client-form" */ '../views/ClientForm.vue')
+  },
+  {
+    meta: {
+      title: 'Edit Client'
+    },
+    path: '/client/:id',
+    name: 'client.edit',
+    component: () => import(/* webpackChunkName: "client-form" */ '../views/ClientForm.vue'),
+    props: true
   }
-];
+]
 
-const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
-});
+const router = new VueRouter({
+  base: process.env.BASE_URL,
+  mode: 'history',
+  routes,
+  scrollBehavior (to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { x: 0, y: 0 }
+    }
+  }
+})
 
-export default router;
+export default router
